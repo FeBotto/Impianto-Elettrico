38758d99d9ad8bd658faf5d513026fad28371008
Butt <121292057+FeBotto@users.noreply.github.com>
2023-09-08 13:21:52 +0200

    impianto finito

--- casa/script.js
+++ casa/script.js
@@ -10,3 +10,36 @@ function conferma(){
         nomeProprietario = txt.value;
     }
 }
+
+function bagno(){
+    document.getElementById("bagno").style.display = "block";
+    setTimeout(togli, 1000);
+}
+
+function camera(){
+    document.getElementById("camera").style.display = "block";
+    setTimeout(togli, 1000);
+}
+
+function soggiorno(){
+    document.getElementById("soggiorno").style.display = "block";
+    setTimeout(togli, 1000);
+}
+
+function cucina(){
+    document.getElementById("cucina").style.display = "block";
+    setTimeout(togli, 1000);
+}
+
+function terrazzo(){
+    document.getElementById("terrazzo").style.display = "block";
+    setTimeout(togli, 1000);
+}
+
+function togli(){
+    document.getElementById("bagno").style.display = "none";
+    document.getElementById("camera").style.display = "none";
+    document.getElementById("soggiorno").style.display = "none";
+    document.getElementById("cucina").style.display = "none";
+    document.getElementById("terrazzo").style.display = "none";
+}
